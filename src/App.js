c8565620fd6c63503765d327e9fa7dfe02711b14
thomas <thomasgrz@outlook.com>
2020-09-08 21:29:29 -0400

    updated splash page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ITS A ME <code>src/App.js</code> and save to reload.
+          This page was created by merging a feature branch in GitHub which triggered a Travis deployment!
         </p>
         <a
           className="App-link"
